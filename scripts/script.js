eb6b41681edcb932c76820ac533e2ad9e2adab31
Muflih Abqori <muflihabqori@gmail.com>
2025-06-03 13:17:15 +0700

    homepage

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+const heroImgMobile = document.querySelector('.hero-img-container img');
+
+function updateHeroImg() {
+  if (window.innerWidth <= 900) {
+    heroImgMobile.src = 'assets/images/gekko-mobile.png';
+  } else {
+    heroImgMobile.src = 'assets/images/gekko-img.png'; // ganti ke gambar desktop
+  }
+}
+
+updateHeroImg();
+
+window.addEventListener('resize', updateHeroImg);
+
+const menu = document.querySelector('#menu-icon');
+const navList = document.querySelector('.nav-list');
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navList.classList.toggle('open');
+}
