ca19d186491fa97f55b4508aa0f5e2d14ef97005
Muflih Abqori <muflihabqori@gmail.com>
2025-06-04 20:07:10 +0700

    adding abilities section

--- scripts/script.js
+++ scripts/script.js
@@ -20,3 +20,14 @@ menu.onclick = () => {
   navList.classList.toggle('open');
   document.documentElement.classList.toggle('no-scroll');
 }
+
+const wingmanText = "A small, yellow creature that is able to appear around Gekko, Wingman's head bears resemblance to that of an axolotl with a dark gray metallic head-plate. He walks in a bipedal fashion and appears to be the most independent of Gekko's gang, and, as his name suggests, is Gekko’s Wingman.";
+const dizzyText = "Dizzy is a small blue armadillo-like creature which floats when idle. When attacking, she summons a bubble around her and fires a blue plasma-like substance out of her tail. The projectile she shoots has a slimy, sticky texture and is blue.";
+const moshpitText = "Gekko can summon Mosh at will and often wields him much like a grenade weapon. Mosh is similar to a frog egg as a sphere of green slimy liquid with large eyes. When used, he will seemingly make an area with more Moshes inside of it, shortly exploding afterward. ";
+const thrashText = "Thrash, a shark-like creature, originaly living in salt water (as seen in abyss) can be directly controlled by Gekko, but is also sentient when uncontrolled. Hovering and floating slightly above the ground, Thrash also seems to be the most shaky out of the bunch, though much like the other summons, still values Gekko's safety above all else.";
+
+document.querySelector('.wingman p').textContent = wingmanText;
+document.querySelector('.dizzy p').textContent = dizzyText;
+document.querySelector('.moshpit p').textContent = moshpitText;
+document.querySelector('.thrash p').textContent = thrashText;
+
