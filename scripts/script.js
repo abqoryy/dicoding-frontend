a2b337668444f3bf605bb7fee9974076198432b5
Muflih Abqori <muflihabqori@gmail.com>
2025-06-06 11:09:24 +0700

    fixing bug

--- scripts/script.js
+++ scripts/script.js
@@ -21,6 +21,20 @@ menu.onclick = () => {
   document.documentElement.classList.toggle('no-scroll');
 }
 
+const navLink = document.querySelectorAll('.nav-list a');
+
+navLink[0].addEventListener('click', closeMenu);
+navLink[1].addEventListener('click', closeMenu);
+navLink[2].addEventListener('click', closeMenu);
+navLink[3].addEventListener('click', closeMenu);
+navLink[4].addEventListener('click', closeMenu);
+
+function closeMenu() {
+  menu.classList.remove('bx-x');
+  navList.classList.remove('open');
+  document.documentElement.classList.remove('no-scroll');
+}
+
 const wingmanText = "A small, yellow creature that is able to appear around Gekko, Wingman's head bears resemblance to that of an axolotl with a dark gray metallic head-plate. He walks in a bipedal fashion and appears to be the most independent of Gekko's gang, and, as his name suggests, is Gekko’s Wingman.";
 const dizzyText = "Dizzy is a small blue armadillo-like creature which floats when idle. When attacking, she summons a bubble around her and fires a blue plasma-like substance out of her tail. The projectile she shoots has a slimy, sticky texture and is blue.";
 const moshpitText = "Gekko can summon Mosh at will and often wields him much like a grenade weapon. Mosh is similar to a frog egg as a sphere of green slimy liquid with large eyes. When used, he will seemingly make an area with more Moshes inside of it, shortly exploding afterward. ";
